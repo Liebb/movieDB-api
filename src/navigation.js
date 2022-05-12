e71346275da621702c114747648f75804ed22fbc
Liebermen <liebermorazan@gmail.com>
2022-05-12 00:34:56 -0600

    Se agrega la navegación dinámica mediante hash y location, se agregan funcionalidades al botón de ver más, buscar y la flecha que regresa al home

--- src/navigation.js
+++ src/navigation.js
@@ -1,7 +1,18 @@
+searchFormBtn.addEventListener('click',() => {
+    location.hash = '#search='
+})
+trendingBtn.addEventListener('click',() => {
+    location.hash = '#trends'
+})
+arrowBtn.addEventListener('click',() => {
+    location.hash = '#home'
+})
+
+
 //Cuando se cargue mi contenido 
 window.addEventListener('DOMContentLoaded', navigator, false);
 //haschange se activa cuando cambia el hash
-window.addEventListener('haschange', navigator, false);
+window.addEventListener('hashchange', navigator, false);
 
 
 function navigator(){
@@ -24,21 +35,85 @@ function navigator(){
 
 function homePage() {
     console.log('Home');
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.add('inactive');
+    headerTitle.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.add('inactive');
+    searchForm.classList.remove('inactive');
+
+    trendingPreviewSection.classList.remove('inactive');
+    categoriesPreviewSection.classList.remove('inactive');
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.add('inactive');
+
     getTrendingMoviesPreview();
     getCategoriesPreview();
 }
 
 function categoriesPage() {
     console.log('Category');
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    headerTitle.classList.add('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.remove('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
 }
 
 function movieDetailsPage() {
     console.log('Movie');
+
+    headerSection.classList.add('header-container--long');
+    // headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.add('header-arrow--white');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.add('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.remove('inactive');
 }
 
 function searchPage() {
     console.log('search!!');
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    headerTitle.classList.add('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.remove('inactive');
+    searchForm.classList.remove('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
 }
 function trendsPage() {
     console.log('TRENDS');
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    headerTitle.classList.add('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.remove('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
 }
